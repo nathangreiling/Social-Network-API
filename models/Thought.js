8a54e9d272ba62745071d0dfdcf51c0564725f87
nathangreiling <ngreiling20@gmail.com>
2022-06-23 20:19:29 -0500

    thoughtschema

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,15 @@
 const { Schema, model,  } = require('mongoose');
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 0,
+        maxLength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: current.timestamp,
+
+    }
+})
